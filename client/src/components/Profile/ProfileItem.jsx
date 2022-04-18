3cbb4636464bfb333e33c367b80dcda736a2e83c
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-04-18 18:47:50 +0000

    hardcode list of users

---
+++ client/src/components/Profile/ProfileItem.jsx
@@ -0,0 +1,7 @@
+export default function ProfileItem(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+  )
+}
--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,7 +1,73 @@
+import ProfileItem from "./ProfileItem"
+
+const users = [
+  {
+  id: 1,
+  name: "Francis",
+  email: "rick.sandchez@gmail.com",
+  password: "picklerick",
+  wins: 0,
+  losses: 0
+  },
+  {
+  id: 2,
+  name: "Billy",
+  email: "lisa.simpson@gmail.com",
+  password: "ehhhhh",
+  wins: 0,
+  losses: 0
+  },
+  {
+  id: 3,
+  name: "Jacob",
+  email: "link@yahoo.com",
+  password: "hyrule",
+  wins: 0,
+  losses: 0
+  },
+  {
+  id: 4,
+  name: "Sally",
+  email: "simon_bel123@mail.ca",
+  password: "dracula",
+  wins: 0,
+  losses: 0
+  },
+  {
+  id: 5,
+  name: "Samantha",
+  email: "all_might@academia.jp",
+  password: "plusUltra",
+  wins: 0,
+  losses: 0
+  },
+  {
+  id: 6,
+  name: "Tommy",
+  email: "mario@mushroomkindom.jp",
+  password: "plumber79",
+  wins: 0,
+  losses: 0
+  }
+  ]
+
+
+
 export default function Profile() {
+
+  const usersInfo = users.map (user => (
+    <ProfileItem
+      key={user.id}
+      name={user.name}
+      wins={user.wins}
+      losses={user.losses}
+    />
+  ))
+
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h1>List of Users</h1>
+      {usersInfo}
     </div>
   )
 }
