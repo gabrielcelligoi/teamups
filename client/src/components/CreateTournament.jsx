48350095e0e802383c379afda2db686dec77a251
Francis <fwang36@gmail.com>
2022-04-18 14:01:08 -0400

    create basic form for tournament creation

--- client/src/components/CreateTournament.jsx
+++ client/src/components/CreateTournament.jsx
@@ -1,10 +1,31 @@
 import './CreateTournament.css'
 
-export default function CreateTournament(props) {
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
+export default function CreateTournament(props) {
+  let location = useLocation();
+
+  const sportsArr = location.state.sports.map(sport => {
+    return sport = sport.name
+  })
   return (
     <div>
       <h1>Create A Tournament</h1>
+      <form>
+        <label htmlFor='create-tournament-name'>Tournament Name: </label> 
+        <input type='text' id='create-tournament-name' name='create-tournament-name' />
+        <label htmlFor='create-tournament-type'>Select Type: </label>
+        <select id="create-tournament-type" name='create-tournament-type'>
+          <option value="Single Elimination">Single Elimination</option>
+        </select>
+        <label htmlFor='create-tournament-players'>Number Of Players: </label>
+        <input type="text" id='create-tournament-players' name='create-tournament-players' />
+        <select id="create-tournament-sport" name='create-tournament-sport'>
+          {sportsArr.map(item => <option key={item} value={item}>{item}</option>)}
+        </select>
+        <button type='submit'>Create</button>
+      </form>
     </div>
 
   )
