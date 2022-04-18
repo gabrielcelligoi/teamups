a2b4cdb089511a3643be727001e60deb1b92964a
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-18 15:20:55 -0400

    create Tournament Item

---
+++ client/src/components/TournamentItem.jsx
@@ -0,0 +1,15 @@
+import './TournamentItem.css';
+import { useState } from 'react';
+export default function TournamentItem(props) {
+
+return ( 
+ <section className="tournament-container">
+    <ul>
+
+      <h3>{props.name}</h3> 
+    </ul> 
+  </section>
+  
+  )
+
+}
