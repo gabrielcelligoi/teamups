4033eaa954bad4364a19f72a9aea1f9e45c0a718
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-18 09:01:40 -0400

    update tournaments data

--- client/src/components/Tournaments.jsx
+++ client/src/components/Tournaments.jsx
@@ -1,90 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import useApplicationData from "../hooks/useApplicationData";
 import { useLocation } from 'react-router-dom';
 
 import './Tournaments.css';
-const tournaments = [
-  {
-    id: 1,
-    name: 'Ultimate Championship',
-    sport_id: 1
-  },
-  {
-    id: 2,
-    name: 'Winner winner chicken dinner',
-    sport_id: 2
-  },
-  {
-    id: 3,
-    name: 'Ultimate Frisbee',
-    sport_id: 1
-  },    
-  {
-    id: 4,
-    name: 'Ultimate Sport!!!!!!!',
-    sport_id: 3
-  },    
-  {
-    id: 5,
-    name: 'Total Tournament',
-    sport_id: 2
-  },
-]
 
-const sports = [
-  {
-    id: 1,
-    name: 'Baseball',
-  },
-  {
-    id: 2,
-    name: 'Hockey',
-  },
-  {
-    id: 3,
-    name: 'Soccer',
-  }
-]
-
-const matches = [
-  {
-    id: 1,
-    name: "matcheroo1",
-    tournament_id: 1,
-    sport_id: 2
-  },
-  {
-    id: 2,
-    name: "matcheroo2",
-    tournament_id: 1,
-    sport_id: 2
-  },
-  {
-    id: 3,
-    name: "matcheroo3",
-    tournament_id: 3,
-    sport_id: 2
-  },
-  {
-    id: 4,
-    name: "matcheroo4",
-    tournament_id: 3,
-    sport_id: 2
-  },
-  {
-    id: 5,
-    name: "matcheroo5",
-    tournament_id: 2,
-    sport_id: 2
-  },
-  {
-    id: 6,
-    name: "matcheroo6",
-    tournament_id: 2,
-    sport_id: 2
-  },
-]
 
 export default function Tournaments(props) {
+  const { state }  = useApplicationData()
+  let location = useLocation();
+  // console.log(location.state)
+  let matches = location.state.match
+  let tournaments = location.state.tournaments
   const [hidden, setHidden] = useState(true);
   const [Tournaments, setTournaments] = useState(null);
   useEffect(() => {
@@ -102,10 +28,12 @@ export default function Tournaments(props) {
   }
 
     const handleMatches = function(event) {
-    event.preventDefault() 
+    event.preventDefault();
     let value = event.target.value;
     if (value !== null) {
       setHidden(setFilteredMatch(filterMatch(value)))
+    } else {
+      setHidden(matches)
     }
   }
 
@@ -114,7 +42,9 @@ export default function Tournaments(props) {
       <h1>Tournaments</h1>
         {Tournaments &&
         Tournaments.map(tournament => (
-         <button key={tournament.id} value={tournament.id} onClick={handleMatches}> {tournament.name} </button>
+         <button key={tournament.id} value={tournament.id} onClick={(e) => {
+          e.preventDefault()
+          handleMatches(e)}}> {tournament.name} </button>
          ))}
       <div hidden={hidden}>
         <h1>Matches in Tournament:</h1>
