843ca9dd411d6f014c9aac889486cdad5282706f
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-04-20 21:10:26 +0000

    fix top images

--- client/src/components/Homepage/SecondaryBanner.jsx
+++ client/src/components/Homepage/SecondaryBanner.jsx
@@ -1,10 +1,19 @@
 import './Banner.scss'
 
+import useApplicationData from '../../hooks/useApplicationData'
+import { Link } from 'react-router-dom'
+
 export default function SecundaryBanner () {
+  const { state }  = useApplicationData()
+
   return (
-    <div className='banner'>
-            
-      <h1 className='front-txt'>Check out other players</h1>
+    <div>
+      <Link to="profiles" state={state} style={{ textDecoration: 'none' }}>
+        <div className='banner'>
+          <h1 className='front-txt'>Check out other players</h1>
+        </div>
+      </Link>
+      
 
     </div>
   )
