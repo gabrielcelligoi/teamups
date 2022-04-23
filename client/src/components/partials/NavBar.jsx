aa0cf9d69160d72331c08e480da59e59ff9e417f
Francis <fwang36@gmail.com>
2022-04-22 20:48:23 -0400

    add login and register form locations to navbar

--- client/src/components/partials/NavBar.jsx
+++ client/src/components/partials/NavBar.jsx
@@ -1,41 +1,74 @@
-
-import styles from './NavBar.scss'
+import './NavBar.scss'
+import { useState } from 'react'
 
 export default function NavBar(props) {
+
+  const [login, setLogin] = useState(false)
+  const [register, setRegister] = useState(false)
+
+  const handleLoginClick = (e) => {
+    e.preventDefault()
+    if (register) {
+      setRegister(false)
+    }
+    setLogin(value => !value)
+    console.log("login", login)
+  }
+
+  const handleRegisterClick = (e) => {
+    e.preventDefault()
+    if (login) {
+      setLogin(false)
+    }
+    setRegister(value => !value)
+    console.log("register", register)
+  }
   return (
-    <nav>
-      <div className='logo'>
-      <a href="/" className='logo-name'>
-        TeamUps
-      </a>
+    <section>
+
+      <nav>
+        <div className='logo'>
+          <a href="/" className='logo-name'>
+            TeamUps
+          </a>
+        </div>
+        <ul>
+
+          <li>
+            <a href="/register" className='navbar-link' onClick={handleRegisterClick}>
+              Register
+            </a>
+          </li>
+
+          <li>
+            <a href="/login" className='navbar-link' onClick={handleLoginClick}>
+              Login
+            </a>
+          </li>
+
+          <li>
+            <a href="/logout" className='navbar-link'>
+              Logout
+            </a>
+          </li>
+
+          <li>
+            <a href="/my-profile" className='navbar-link'>
+              My Profile
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {login ? 
+      <div>
+        <h1> Login test</h1>
       </div>
-      <ul>
-
-        <li>
-          <a href="/register" className='navbar-link'>
-            Register
-          </a>          
-        </li>
-
-        <li>
-          <a href="/login" className='navbar-link'>
-            Login
-          </a>          
-        </li>
-
-        <li>
-          <a href="/logout" className='navbar-link'>
-            Logout
-          </a>          
-        </li>
-
-        <li>
-          <a href="/my-profile" className='navbar-link'>
-            My Profile
-          </a>          
-        </li>
-
-      </ul>
-    </nav>
+      : null}
+      {register ?
+      <div>
+        <h1>Register Test</h1>
+      </div>
+      : null}
+    </section>
   )
 }
