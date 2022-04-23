321aecf24c63479bdbfe10bef670cfa42e7e8a87
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-23 10:08:59 -0400

    add className to img

--- client/src/components/MyProfile/ShowName.jsx
+++ client/src/components/MyProfile/ShowName.jsx
@@ -1,12 +1,15 @@
+import './styles.scss'
+
 export default function ShowName(props) {
   return (
     <section>
       <h1>Name:</h1>
       <h2>{props.name}</h2>
       <img 
+      className='img'
       src='https://upload.wikimedia.org/wikipedia/commons/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg'
-      alt='EditName'
-      onClick={props.onEditName}
+      alt='Edit'
+      onClick={props.onEdit}
       />
     </section>
   )
