c3083efa0d7153956a58a7b15723c22da34735cb
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-23 15:05:23 -0400

    filter out add sports options to those that are not already in userSports

--- client/src/components/MyProfile/AddSports.jsx
+++ client/src/components/MyProfile/AddSports.jsx
@@ -12,20 +12,49 @@ export default function AddSports(props) {
     reset();
     props.onCancel();
   }
-  const mappedSports = props.sportsObject.map(sport => {
 
-    return (
-      <img
-      className='sports--img'
-      src={sport.image}
-      />
-    )
+  let id = function(sportsObject) {
+    let result = [];
+    for(let sport of sportsObject) {
+      result.push(sport.id)
+    }
+    return result;
+  }
+
+ const sportsId = id(props.sportsObject)
+  const userSports = props.userSports;
+
+  let filtered = sportsId.filter(item => !userSports.includes(item));
+
+  console.log(filtered)
+
+  const showMySports = function(idArray, sportsObject) {
+    let result = [];
+    for (let number of idArray) {
+      for (let sport of sportsObject) {
+        if (number === sport.id) {
+          result.push(sport);
+        }
+      }
+    }
+    return result;
+  }
+
+  const sportsResult = showMySports(filtered, props.sportsObject)
+
+  const mappedSports = sportsResult.map(sport => {
+    return <img className='sports--img'
+    src={sport.image}
+    />
   })
+
   return (
-    <section>
+    <section className='profile'>
       <h1>Add Sports:</h1>
+      <div className='edit'>
       <h2>{mappedSports}</h2>
       <button onClick={cancel}>Cancel</button>
+      </div>
     </section>
   )
 }
