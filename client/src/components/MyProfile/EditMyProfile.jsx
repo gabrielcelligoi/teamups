65285feb61f25ffd0922b7cee832724e77482684
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-23 10:13:47 -0400

    add add email edit option to edit my profile

--- client/src/components/MyProfile/EditMyProfile.jsx
+++ client/src/components/MyProfile/EditMyProfile.jsx
@@ -1,5 +1,7 @@
 import ShowName from "./ShowName";
 import FormName from "./FormName";
+import ShowEmail from "./ShowEmail"
+import FormEmail from "./FormEmail"
 import { useLocation } from "react-router-dom";
 import useVisualMode from "../../hooks/useVisualMode";
 
@@ -8,6 +10,7 @@ export default function EditMyProfile(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const EDITNAME = "EDITNAME"
+  const EDITEMAIL = "EDITEMAIL"
   const location = useLocation();
   const user = location.state.users[0];
   const { mode, transition, back } = useVisualMode(
@@ -19,14 +22,26 @@ export default function EditMyProfile(props) {
       {mode === SHOW && (
         <ShowName
         name={user.name}
-        onEditName={() => transition(EDITNAME)}
+        onEdit={() => transition(EDITNAME)}
         />
       )}
       {mode === EDITNAME && (
         <FormName 
         name={user.name}
         onCancel={back}
+        onSave={() => transition(SHOW)}
         />
+      )}
+      {mode === SHOW && (
+        <ShowEmail 
+        email={user.email}
+        onEdit={() => transition(EDITEMAIL)}/>
+      )}
+      {mode === EDITEMAIL && (
+        <FormEmail 
+        email={user.email}
+        onCancel={back}
+        onSave={() => transition(SHOW)}/>
       )}
     </section>
   )
