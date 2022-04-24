566ef8252c23fa5472df036fe49488b048ebacb4
Keila Kopvillem <kmkopvillem@gmail.com>
2022-04-23 23:13:36 -0400

    changed tag

--- client/src/components/MyProfile/ShowPassword.jsx
+++ client/src/components/MyProfile/ShowPassword.jsx
@@ -15,7 +15,7 @@ export default function ShowPassword(props) {
     <section className='profile'>
     <h1>Password:</h1>
     <div className='edit'>
-    <h4>{length(props.password)}</h4>
+    <div>{length(props.password)}</div>
     <img 
     className="img"
     src='https://upload.wikimedia.org/wikipedia/commons/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg'
