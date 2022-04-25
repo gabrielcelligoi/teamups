eecd429c20bb8123fe38fb17a8e7886c83df28da
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-04-25 13:26:27 +0000

    add containers

--- client/src/components/messenger/Messenger.jsx
+++ client/src/components/messenger/Messenger.jsx
@@ -3,7 +3,24 @@ import './Messenger.scss';
 export default function Messenger() {
   return (
     <div className='messenger'>
-      hello  
+
+      <div className="chatMenu">
+        <div className="chatMenuWrapper">
+          menu  
+        </div>
+      </div>
+
+      <div className="chatBox">
+        <div className="chatBoxWrapper">
+          box  
+        </div>
+      </div>
+      
+      <div className="chatOnline">
+        <div className="chatOnlineWrapper">
+          online  
+        </div>
+      </div> 
     </div>
   )
 }
